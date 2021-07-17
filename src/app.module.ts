bd86a12419d93b8592a0dba3b30d0f8ddf35a989
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2021-07-17 16:16:37 +0100

    🗃️ configure postgres database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfig } from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [TypeOrmModule.forRoot(TypeOrmConfig), TasksModule],
 })
 export class AppModule {}
