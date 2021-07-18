144d17d885067e1cf6bd2eb94d7a8f0486390c5d
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2021-07-18 14:18:40 +0100

    ✨ implement Signin feature

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,4 +12,9 @@ export class AuthController {
   ): Promise<void> {
     return this.authService.signUp(authCredentialsDto);
   }
+
+  @Post('/signin')
+  signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto) {
+    return this.authService.signIn(authCredentialsDto);
+  }
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -17,7 +17,7 @@ export class UserRepository extends Repository<User> {
     user.salt = await bcrypt.genSalt(10);
     user.password = await this.hashPassword(password, user.salt);
     try {
-      // await user.save();
+      await user.save();
     } catch (error) {
       // 23505: Duplicate username code error
       if (error.code === 23505) {
@@ -26,6 +26,18 @@ export class UserRepository extends Repository<User> {
         throw new InternalServerErrorException();
       }
     }
+  }
+
+  async validateUserPassword(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<string> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.findOne({ username });
+
+    if (user && (await user.validatePassword(password))) {
+      return user.username;
+    }
+    return null;
   }
 
   private async hashPassword(password: string, salt: string): Promise<string> {
