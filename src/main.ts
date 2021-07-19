6372755e8e8ffeacf9ac5f4b5c2e2ab61a28348f
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2021-07-19 10:19:56 +0100

    ✨ accept cors in development

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,13 @@ import * as config from 'config';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
-
   const app = await NestFactory.create(AppModule);
   const serverConfig = config.get('server');
+
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  }
+
   const PORT = process.env.PORT || serverConfig.port;
   await app.listen(PORT);
 
